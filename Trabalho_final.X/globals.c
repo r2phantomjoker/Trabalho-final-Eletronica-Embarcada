8075291e7bac8226bce4aa9146ad4dd27c573e11
r2phantomjoker <Arthurpinheiromarinho@gmail.com>
2025-11-27 11:27:23 -0300

    feat: cria estrutura de variáveis globais e estados do sistema
    Implementação dos arquivos globals.c e globals.h para gerenciamento de estado compartilhado:
    
    - Criação das variáveis de telemetria exigidas (andar_atual, posicao_mm, velocidade_atual, temperatura_ponte) conforme requisitos do projeto.
    - Definição de constantes de controle (MOTOR_SUBINDO, MOTOR_PARADO).
    - Configuração de variáveis como 'volatile' e 'extern' para acesso seguro entre módulos (Motor, Bluetooth e Lógica).

---
+++ Trabalho_final.X/globals.c
@@ -0,0 +1,20 @@
+/*
+ * Arquivo: globals.c
+ * Descrição: Alocação real de memória das variáveis globais
+ * Autor: Arthur Marinho
+ */
+
+#include "globals.h"
+
+
+// Variáveis de Telemetria 
+volatile uint8_t andar_atual = 0;       // Inicia no Térreo (0)
+volatile uint8_t andar_destino = 0;     // Sem destino inicial
+volatile uint8_t estado_motor = MOTOR_PARADO; 
+volatile uint8_t posicao_mm = 0;        // 0 mm (Térreo)
+volatile float velocidade_atual = 0.0;  // 0.0 mm/s
+volatile float temperatura_ponte = 0.0; // 0.0 °C
+
+// Vetor de Solicitações (Botões apertados)
+// [0]=Térreo, [1]=1ºAndar, [2]=2ºAndar, [3]=3ºAndar
+volatile bool solicitacoes[4] = {false, false, false, false};
